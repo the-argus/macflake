68d196a36cb4efe7a64ce4669ee0fffde68f0822
Ian McFarlane <ian.mcfarlane@kdab.com>
2025-06-18 16:04:42 -0400

    aarch64 darwin

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
 	environment.systemPackages = with pkgs; [
 		neovim
 		vim
+		git
 		ripgrep
 		fd
 		tldr
@@ -19,7 +20,6 @@
 
 	# Create /etc/zshrc that loads the nix-darwin environment.
 	programs.zsh.enable = true;  # default shell on catalina
-	# programs.fish.enable = true;
 
 	# Set Git commit hash for darwin-version.
 	system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -29,5 +29,5 @@
 	system.stateVersion = 4;
 
 	# The platform the configuration will be used on.
-	nixpkgs.hostPlatform = "x86_64-darwin";
+	nixpkgs.hostPlatform = "aarch64-darwin";
 }
